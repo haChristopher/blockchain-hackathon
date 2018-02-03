86e730a810075e4038edc8de9aa324f57ae37861
katarina.preikschat <k-preikschat@t-online.de>
2018-02-03 20:13:52 +0100

    pet shop tutorial + first draft smart contract

---
+++ contracts/Vehicledata.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.4.17;
+
+contract Vehicledata {
+
+struct VehicleDataEntry {
+    uint sensorData;
+    uint timestamp;
+}
+
+mapping(address => VehicleDataEntry[]) public vehicleDataEntries;
+// mapping(bytes32 => address) public mappedVinToKeyEntries;
+address owner;
+
+function Vehicledata() public {
+    owner = msg.sender;
+}
+
+function storeSensorData(uint inputSensorData, uint inputTimestamp) public {
+    var vehicleIdentity = msg.sender;
+    vehicleDataEntries[vehicleIdentity].push(VehicleDataEntry({
+        sensorData: inputSensorData,
+        timestamp: inputTimestamp
+    }));
+}
+
+
+
+}
